05092829cca4adaae4d798c27d091f4b3d1968b0
Harvey Sanders <sanders.harvey@gmail.com>
2014-12-15 06:25:04 +0000

    fix table show in view.js; make new people.json; previous model.js update fixed problem

--- view.js
+++ view.js
@@ -48,7 +48,7 @@ function makeTable(headers) {
         show: function(values) {
             var table = createFormattedTable(headers);
             table.push.apply(table, values);
-            console.log(table);
+            console.log(table.toString());
         }
     };
     return _table;
