b4c27f6966ed9ff464b08907a04e2ee26516e9f8
Harvey Sanders <sanders.harvey@gmail.com>
2014-12-15 02:54:26 +0000

    hide master folder in file system. complete TODO9

--- app.js
+++ app.js
@@ -3,24 +3,32 @@
 'use-strict';
 
 // TODO 4 : require the model module //
-
+const
+    model = require('./model.js'),
+    
     
     // TODO 6 : require the collections and exit utility modules //
-    
+    collections = require('./util/collections'),
+    exit = require('./util/exit');
     // TODO 16 : require the view, prompt, and controller modules //
     
     
 // TODO 5 : using the model factory, make a person object and print it to the console //
-
+var neil = model.makePerson("Neil", "deGrasse Tyson", "ndgTyson@amnh.org", "212-769-5901", "Male", "10/5/1958");
+console.log(neil);
 
 // TODO 8 : create a list object that will manage a collection of people //
+var people = collections.makeList("people.json", model.makePersonFromJSON);
 
 
 // TODO 9 : add the me person object to the people list //
-
+people.add(neil);
 
 // TODO 10 configure the exit utility to fire the list's saveSync method //
-
+exit.init( {
+    exit: {actions: [people.saveSync.bind(people)]},
+    sigint: {actions: [people.saveSync.bind(people)]}
+});
 
 // TODO 17 : initialize the controller, passing in the required model and views //
 
--- model.js
+++ model.js
@@ -1,10 +1,26 @@
 'use-strict';
 
 // TODO 1 : create the makePerson factory //
-
+function makePerson(firstName, lastName, email, telephone, gender, birthDate) {
+    var _person = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        telephone: telephone,
+        gender: gender,
+        birthDate: birthDate,
+        
+        name:   function() {
+            return _person.firstName + ' ' + _person.lastName;
+        }
+    };
+    return _person;
+};
 
 // TODO 2: expose the makePerson factory //
-
+module.exports.makePerson = makePerson;
 
 // TODO 3: create the makePersonFromJSON method and expose it 
-
+module.exports.makePersonFromJSON = function (data) {
+    return makePerson(data.firstName, data.lastName , data.email, data.telephone, data.gender, data.birthDate);
+}
